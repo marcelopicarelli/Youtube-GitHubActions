bff0ee31c5775d1163e0b00e207826c9368d7634
Marcelo Picarelli <marcelopicarelli@natura.net>
2023-07-25 12:46:59 -0300

    feat: test

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,7 +4,7 @@ describe("Math", () => {
     it("Should be able to sum A and B", () => {
         expect(sum(2,2)).toBe(4);
     });
-    it("Should be fail", () => {
-        expect(sum(2,2)).toBe(5);
+    it("Should be able to sum 2 and 3", () => {
+        expect(sum(2,3)).toBe(5);
     });
 });
